cc3de4d00ec1edd25bd6b92f0cb205aad0d9c348
K <dev7kevin@gmail.com>
2021-07-13 10:32:35 -0700

    LivingEntity class for NPC

---
+++ Assets/Scripts/LivingEntity.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class LivingEntity : MonoBehaviour, IDamageable
+{
+    public float startingHealth = 100f;
+    public float health {get; protected set;}
+    public bool dead {get; protected set;}
+
+    public event Action OnDeath;
+
+    private const float minTimeBetDamaged = 0.1f;
+    private float lastDamagedTime;
+
+    protected bool IsInvulnerable
+    {
+        get
+        {
+            if(Time.time >= lastDamagedTime + minTimeBetDamaged) return false;
+
+            return true;
+        }
+    }
+
+    protected virtual void OnEnable()
+    {
+        dead = false;
+        health = startingHealth;
+    }
+
+    public virtual bool ApplyDamage(DamageMessage damageMessage)
+    {
+        if(IsInvulnerable || damageMessage.damager == gameObject || dead) return false;
+
+        lastDamagedTime = Time.time;
+
+        health -= damageMessage.amount;
+
+        if(health <= 0) Die();
+
+        return true;
+    }
+
+    public virtual void RestoreHealth(float newHealth)
+    {
+        if(dead) return;
+
+        health += newHealth;
+    }
+
+    public virtual void Die()
+    {
+        if(OnDeath != null) OnDeath();
+
+        dead = true;
+    }
+}
